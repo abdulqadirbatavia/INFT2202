1b64e0ee4252069478cbf606de2775afc403e999
abdulqadirbatavia <qadirbatavia@gmail.com>
2025-03-28 22:47:10 -0400

    test3_complete
    test3

---
+++ assi1/server/product/product.js
@@ -0,0 +1,18 @@
+// Name: Abdul Qadir Batavia
+// Student ID: 100928644
+// Course: Web Development - CSS
+// File: product.js
+// Description: This file defines the Product constructor function for the product shop.
+
+
+// server/app/products/product.js
+function Product(name, description, stock, price, owner = null) {
+    this.name = name;
+    this.description = description;
+    this.stock = stock;
+    this.price = price;
+    this.owner = owner;
+    this.createdAt = new Date().toISOString();
+}
+
+export default Product;
